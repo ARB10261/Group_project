d20e91fb19a983bd5370e35cc8bc7b0dcff8c56e
Dowlapalli Ramu <159682571+Ramudowlapalli@users.noreply.github.com>
2025-11-19 22:14:46 +0530

    Contact page updated

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import FooterSection from './Landingpage/FooterSection';
 import LandingPage from './Landingpage/LandingPage';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import About from './Aboutpage/About';
-import Contact from './Contactpage/Contact';
+import Cards from './Contactpage/Cards';
 import Department from './Departments/Department';
 import Service from './Servicespage/Service';
 import LoginPage from "./LoginPage";
@@ -19,7 +19,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
+        <Route path="/contact" element={<Cards />} />
         <Route path="/department" element={<Department />} />
         <Route path="/service" element={<Service />} />
         <Route path="/login" element={<LoginPage />} />
